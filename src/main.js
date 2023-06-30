4e50702f4c8c98c4c53fb8896b98f7cc9f787a97
FredyBarajas <119268313+FredyBarajas@users.noreply.github.com>
2023-06-30 18:25:04 -0500

    change files

--- src/main.js
+++ src/main.js
@@ -15,191 +15,191 @@ let selectedCountry = '';
 
 // Función para obtener la lista de países desde la API de Ticketmaster
 function fetchCountries() {
-	const url = `https://app.ticketmaster.com/discovery/v2/countries.json?apikey=${apiKey}`;
+  const url = `https://app.ticketmaster.com/discovery/v2/countries.json?apikey=${apiKey}`;
 
-	return fetch(url)
-		.then(response => response.json())
-		.then(data => data._embedded.countries);
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data._embedded.countries);
 }
 
 // Función para generar las opciones del selector de países
 function generateCountryOptions(countries) {
-	countrySelect.innerHTML = '';
+  countrySelect.innerHTML = '';
 
-	const allOption = document.createElement('option');
-	allOption.value = '';
-	allOption.textContent = 'Todos los países';
-	countrySelect.appendChild(allOption);
+  const allOption = document.createElement('option');
+  allOption.value = '';
+  allOption.textContent = 'Todos los países';
+  countrySelect.appendChild(allOption);
 
-	countries.forEach(country => {
-		const option = document.createElement('option');
-		option.value = country.countryCode;
-		option.textContent = country.name;
-		countrySelect.appendChild(option);
-	});
+  countries.forEach(country => {
+    const option = document.createElement('option');
+    option.value = country.countryCode;
+    option.textContent = country.name;
+    countrySelect.appendChild(option);
+  });
 }
 
 // Llamar a la función fetchCountries para obtener la lista de países
 fetchCountries()
-	.then(countries => generateCountryOptions(countries))
-	.catch(error => console.error(error));
+  .then(countries => generateCountryOptions(countries))
+  .catch(error => console.error(error));
 
 // Función para obtener eventos de la API de Ticketmaster
 function fetchEvents(page) {
-	let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&size=${eventsPerPage}&page=${page}`;
+  let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&size=${eventsPerPage}&page=${page}`;
 
-	if (searchText !== '') {
-		url += `&keyword=${encodeURIComponent(searchText)}`;
-	}
+  if (searchText !== '') {
+    url += `&keyword=${encodeURIComponent(searchText)}`;
+  }
 
-	if (selectedCountry !== '') {
-		url += `&countryCode=${selectedCountry}`;
-	}
+  if (selectedCountry !== '') {
+    url += `&countryCode=${selectedCountry}`;
+  }
 
-	return fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			const events = data._embedded.events;
-			totalEvents = data.page.totalElements;
-			totalPages = data.page.totalPages;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const events = data._embedded.events;
+      totalEvents = data.page.totalElements;
+      totalPages = data.page.totalPages;
 
-			return events;
-		});
+      return events;
+    });
 }
 
 // Función para generar el HTML de los pósters de los eventos
 function generatePosters(events) {
-	postersContainer.innerHTML = '';
+  postersContainer.innerHTML = '';
 
-	events.forEach(event => {
-		const poster = document.createElement('div');
-		poster.className = 'poster';
+  events.forEach(event => {
+    const poster = document.createElement('div');
+    poster.className = 'poster';
 
-		const card = document.createElement('div');
-		card.className = 'card';
+    const card = document.createElement('div');
+    card.className = 'card';
 
-		const image = document.createElement('img');
-		image.src = event.images[0].url;
-		image.alt = event.name;
-		card.appendChild(image);
+    const image = document.createElement('img');
+    image.src = event.images[0].url;
+    image.alt = event.name;
+    card.appendChild(image);
 
-		const eventInfo = document.createElement('div');
-		eventInfo.className = 'event-info';
+    const eventInfo = document.createElement('div');
+    eventInfo.className = 'event-info';
 
-		const eventName = document.createElement('h3');
-		eventName.textContent = event.name;
-		eventInfo.appendChild(eventName);
+    const eventName = document.createElement('h3');
+    eventName.textContent = event.name;
+    eventInfo.appendChild(eventName);
 
-		const eventDate = document.createElement('p');
-		eventDate.innerHTML = `<i class="fa fa-calendar"></i> Fecha: ${event.dates.start.localDate}`;
-		eventInfo.appendChild(eventDate);
+    const eventDate = document.createElement('p');
+    eventDate.innerHTML = `<i class="fa fa-calendar"></i> Fecha: ${event.dates.start.localDate}`;
+    eventInfo.appendChild(eventDate);
 
-		const eventLocation = document.createElement('p');
-		eventLocation.innerHTML = `<i class="fa fa-map-marker"></i> Ubicación: ${event._embedded.venues[0].name}`;
-		eventInfo.appendChild(eventLocation);
+    const eventLocation = document.createElement('p');
+    eventLocation.innerHTML = `<i class="fa fa-map-marker"></i> Ubicación: ${event._embedded.venues[0].name}`;
+    eventInfo.appendChild(eventLocation);
 
-		card.appendChild(eventInfo);
-		poster.appendChild(card);
+    card.appendChild(eventInfo);
+    poster.appendChild(card);
 
-		postersContainer.appendChild(poster);
+    postersContainer.appendChild(poster);
 
-		// Agregar evento de clic para mostrar información del evento
-		poster.addEventListener('click', () => {
-			showEventInfo(event.id);
-		});
+    // Agregar evento de clic para mostrar información del evento
+    poster.addEventListener('click', () => {
+      showEventInfo(event.id);
+    });
 
-		poster.addEventListener('click', () => {
-			showEventInfo(event.id);
-			showModal(event);
-		});
-	});
+    poster.addEventListener('click', () => {
+      showEventInfo(event.id);
+      showModal(event);
+    });
+  });
 }
 
 // Función para generar la paginación
 
 function generatePagination() {
-	paginationContainer.innerHTML = '';
+  paginationContainer.innerHTML = '';
 
-	// Calcular el rango de páginas que se mostrarán
-	let startPage = Math.max(2, currentPage - 2);
-	let endPage = Math.min(totalPages, currentPage + 9);
+  // Calcular el rango de páginas que se mostrarán
+  let startPage = Math.max(2, currentPage - 2);
+  let endPage = Math.min(totalPages, currentPage + 9);
 
-	// Mostrar el enlace a la primera página
-	const firstPageLink = createPageLink(1);
-	paginationContainer.appendChild(firstPageLink);
+  // Mostrar el enlace a la primera página
+  const firstPageLink = createPageLink(1);
+  paginationContainer.appendChild(firstPageLink);
 
-	// Mostrar los puntos suspensivos si hay más de 3 páginas
-	if (startPage > 2) {
-		paginationContainer.appendChild(createEllipsis());
-	}
+  // Mostrar los puntos suspensivos si hay más de 3 páginas
+  if (startPage > 2) {
+    paginationContainer.appendChild(createEllipsis());
+  }
 
-	// Generar los enlaces de las páginas
-	for (let i = startPage; i <= endPage; i++) {
-		const pageLink = createPageLink(i);
-		paginationContainer.appendChild(pageLink);
-	}
+  // Generar los enlaces de las páginas
+  for (let i = startPage; i <= endPage; i++) {
+    const pageLink = createPageLink(i);
+    paginationContainer.appendChild(pageLink);
+  }
 
-	// Mostrar los puntos suspensivos si hay más páginas después de la página actual
-	if (endPage < totalPages - 1) {
-		paginationContainer.appendChild(createEllipsis());
-	}
+  // Mostrar los puntos suspensivos si hay más páginas después de la página actual
+  if (endPage < totalPages - 1) {
+    paginationContainer.appendChild(createEllipsis());
+  }
 
-	// Mostrar el enlace a la última página
-	const lastPageLink = createPageLink(totalPages);
-	paginationContainer.appendChild(lastPageLink);
+  // Mostrar el enlace a la última página
+  const lastPageLink = createPageLink(totalPages);
+  paginationContainer.appendChild(lastPageLink);
 }
 
 // Función para crear un enlace de página
 function createPageLink(pageNumber) {
-	const pageLink = document.createElement('a');
-	pageLink.href = '#';
-	pageLink.textContent = pageNumber;
+  const pageLink = document.createElement('a');
+  pageLink.href = '#';
+  pageLink.textContent = pageNumber;
 
-	if (pageNumber === currentPage) {
-		pageLink.classList.add('active');
-	}
+  if (pageNumber === currentPage) {
+    pageLink.classList.add('active');
+  }
 
-	pageLink.addEventListener('click', () => {
-		currentPage = pageNumber;
-		fetchAndRenderEvents();
-	});
+  pageLink.addEventListener('click', () => {
+    currentPage = pageNumber;
+    fetchAndRenderEvents();
+  });
 
-	return pageLink;
+  return pageLink;
 }
 
 // Función para crear los puntos suspensivos
 function createEllipsis() {
-	const ellipsis = document.createElement('span');
-	ellipsis.textContent = '...';
-	ellipsis.classList.add('ellipsis');
-	return ellipsis;
+  const ellipsis = document.createElement('span');
+  ellipsis.textContent = '...';
+  ellipsis.classList.add('ellipsis');
+  return ellipsis;
 }
 
 // Función para mostrar información detallada del evento
 function showEventInfo(eventId) {
-	const url = `https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=${apiKey}`;
+  const url = `https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=${apiKey}`;
 
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			const event = data;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const event = data;
 
-			const modalOverlay = document.getElementById('modal-overlay');
-			const modalImage = document.getElementById('modal-image');
-			const modalImageCircular = document.getElementById(
-				'modal-image-circular'
-			);
-			const modalContent = document.getElementById('modal-content');
+      const modalOverlay = document.getElementById('modal-overlay');
+      const modalImage = document.getElementById('modal-image');
+      const modalImageCircular = document.getElementById(
+        'modal-image-circular'
+      );
+      const modalContent = document.getElementById('modal-content');
 
-			modalImage.src = event.images[0].url;
-			modalImage.alt = event.name;
+      modalImage.src = event.images[0].url;
+      modalImage.alt = event.name;
 
-			modalOverlay.style.display = 'block';
+      modalOverlay.style.display = 'block';
 
-			const venue = event._embedded.venues[0];
-			const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${venue.location.latitude},${venue.location.longitude}`;
+      const venue = event._embedded.venues[0];
+      const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${venue.location.latitude},${venue.location.longitude}`;
 
-			modalContent.innerHTML = `
+      modalContent.innerHTML = `
         <span id="modal-close">&times;</span>
         <img id="modal-image-circular" src="${event.images[0].url}" alt="${event.name}" />
         <div id= "modal-upper"><img id="modal-image" src="${event.images[0].url}" alt="${event.name}" />
@@ -214,46 +214,46 @@ function showEventInfo(eventId) {
         <button class="btn btn-author" onclick="window.open('${event._embedded.attractions[0].url}', '_blank')">More about this author</button>
           
       `;
-			eventInfoContainer.innerHTML = '';
-		});
+      eventInfoContainer.innerHTML = '';
+    });
 
-	// Agrega el siguiente bloque de código al final de la función showEventInfo
-	document.addEventListener('click', event => {
-		const modalOverlay = document.getElementById('modal-overlay');
+  // Agrega el siguiente bloque de código al final de la función showEventInfo
+  document.addEventListener('click', event => {
+    const modalOverlay = document.getElementById('modal-overlay');
 
-		if (event.target === modalOverlay) {
-			modalOverlay.style.display = 'none';
-		}
-	});
+    if (event.target === modalOverlay) {
+      modalOverlay.style.display = 'none';
+    }
+  });
 }
 // Función para realizar la búsqueda de eventos
 function searchEvents() {
-	searchText = searchInput.value.trim();
-	currentPage = 1;
-	fetchAndRenderEvents();
+  searchText = searchInput.value.trim();
+  currentPage = 1;
+  fetchAndRenderEvents();
 }
 
 // Función para cambiar el país seleccionado
 function changeCountry() {
-	selectedCountry = countrySelect.value;
-	currentPage = 1;
-	fetchAndRenderEvents();
+  selectedCountry = countrySelect.value;
+  currentPage = 1;
+  fetchAndRenderEvents();
 }
 
 // Función para realizar la solicitud a la API y mostrar los eventos
 function fetchAndRenderEvents() {
-	fetchEvents(currentPage).then(events => {
-		generatePosters(events);
-		generatePagination();
-	});
+  fetchEvents(currentPage).then(events => {
+    generatePosters(events);
+    generatePagination();
+  });
 }
 
 // Agregar eventos a los elementos de búsqueda y selector de país
 searchButton.addEventListener('click', searchEvents);
 searchInput.addEventListener('keydown', event => {
-	if (event.keyCode === 13) {
-		searchEvents();
-	}
+  if (event.keyCode === 13) {
+    searchEvents();
+  }
 });
 countrySelect.addEventListener('change', changeCountry);
 
